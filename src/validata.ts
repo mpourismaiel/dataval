9d162f4bd0c737d686937f32c7430e6f11c09738
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2019-04-18 19:57:46 +0430

    Add initial functionality

--- src/validata.ts
+++ src/validata.ts
@@ -1,6 +1,88 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
-
+export type Dictionary<T> = { [key: string]: T }
+export type Config = {
+  form?: string
+  rules?: { [key: string]: string }
+  validators?: Dictionary<Validator>
 }
+
+export type Validation = {
+  valid: boolean
+  errors: { key: string; message: string[] }[]
+}
+
+export type Validator = (attrs: {
+  key: string
+  value: string | boolean | number
+  form: string
+  args: string[]
+}) => boolean
+
+export type Rule = { name: string; args: string[] }[]
+
+export interface ValidataInstance {
+  form: string
+  rules: Dictionary<Rule>
+  validators: Dictionary<Validator>
+  add: (name: string, validator: Validator) => ValidataInstance
+  validate: (values: { [key: string]: string | boolean | number }) => Validation
+}
+
+const validators: Dictionary<Validator> = {
+  required: ({ value }) =>
+    typeof value !== 'undefined' &&
+    ((typeof value === 'string' || typeof value === 'number') && (value + '').length > 0),
+  length: ({ value, args: [from, to] }) =>
+    (value + '').length >= parseInt(from) &&
+    (value + '').length <= (parseInt(to) || (value + '').length)
+}
+
+const Validata = (config?: Config): ValidataInstance => {
+  const validata: ValidataInstance = {
+    form: config.form,
+    rules: Object.keys(config.rules).reduce((tmp, key) => {
+      tmp[key] = config.rules[key]
+        .split('|')
+        .map(rule => ({ name: rule.split(':')[0], args: rule.split(':').slice(1) }))
+      return tmp
+    }, {}),
+    validators: config.validators,
+    add: (name, fn) =>
+      Validata({
+        form: config.form,
+        rules: Object.keys(validata.rules).reduce((tmp, k) => {
+          tmp[k] = validata.rules[k].map(rules => `${rules.name}:${rules.args.join(':')}`).join('|')
+          return tmp
+        }, {}),
+        validators: { ...validata.validators, [name]: fn }
+      }),
+    validate: values => {
+      return Object.keys(values).reduce(
+        (result: Validation, key: string) => {
+          const fieldResults = (validata.rules[key] || []).filter(validation => {
+            const isValid = (validators[validation.name] || validata.validators[validation.name])({
+              key,
+              value: values[key],
+              form: validata.form,
+              args: validation.args
+            })
+
+            return !isValid
+          })
+          if (fieldResults.length > 0) {
+            result.valid = false
+            result.errors.push({ key, message: fieldResults.map(v => v.name) })
+          }
+          return result
+        },
+        {
+          valid: true,
+          errors: []
+        }
+      )
+    }
+  }
+
+  return validata
+}
+
+export default Validata
--- test/validata.test.ts
+++ test/validata.test.ts
@@ -1,14 +1,84 @@
-import DummyClass from "../src/validata"
+import Validata from '../src/validata'
 
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
+describe('Validata', () => {
+  const key = 'nationalCode'
+  const value = '1234567890'
+  const form = 'auth'
+
+  const db = { [key]: [value] }
+
+  it('works with built-in rules', () => {
+    const validata = Validata({
+      form,
+      rules: {
+        [key]: 'required'
+      }
+    })
+
+    expect(
+      validata.validate({
+        [key]: value
+      })
+    ).toEqual({ valid: true, errors: [] })
   })
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  it('works with custom rules', () => {
+    const validata = Validata({
+      form,
+      rules: {
+        [key]: 'unique'
+      }
+    }).add('unique', ({ key: k, value: v }) => db[k].indexOf(v) === -1)
+
+    expect(
+      validata.validate({
+        [key]: value + Math.random()
+      })
+    ).toEqual({ valid: true, errors: [] })
+  })
+
+  it('works with combination of custom and built-in rules', () => {
+    const validata = Validata({
+      form,
+      rules: {
+        [key]: 'unique|required'
+      }
+    }).add('unique', ({ key: k, value: v }) => db[k].indexOf(v) === -1)
+
+    expect(
+      validata.validate({
+        [key]: value + Math.random()
+      })
+    ).toEqual({ valid: true, errors: [] })
+  })
+
+  it('returns all errors for invalid values', () => {
+    const validata = Validata({
+      form,
+      rules: {
+        [key]: 'isNumber|required'
+      }
+    }).add('isNumber', ({ value: v }) => typeof v === 'number')
+
+    expect(
+      validata.validate({
+        [key]: ''
+      })
+    ).toEqual({ valid: false, errors: [{ key, message: ['isNumber', 'required'] }] })
+  })
+
+  it('accepts args', () => {
+    const validata = Validata({
+      form,
+      rules: {
+        [key]: `length:${value.length}:${value.length}`
+      }
+    })
+
+    expect(
+      validata.validate({
+        [key]: value
+      })
+    ).toEqual({ valid: true, errors: [] })
   })
 })
